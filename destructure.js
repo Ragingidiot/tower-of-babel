fc03606cb9dcccc9fb16efe82ad4653b6c2326bb
MKH103-14 <MKH103-14@lbcc.linnbenton.edu>
2016-11-21 11:31:33 -0800

    Finished Destructuring

---
+++ destructure.js
@@ -0,0 +1,18 @@
+var json = {
+  "name": {
+    "first": "Yosuke",
+    "family": process.argv[2]
+  },
+  "birth": {
+    "year": 1982,
+    "month": 12,
+    "day": process.argv[3]
+  }
+};
+
+let {name: {family: familyName}, birth: {day: birthDay}} = json;
+
+// Your code here.
+
+console.log(familyName);
+console.log(birthDay);
